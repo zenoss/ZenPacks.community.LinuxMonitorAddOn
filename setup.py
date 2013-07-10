8d52b09ab63da0430d7ce72194f826ad64978114
Eric Edgar <eedgar@zenoss.com>
2013-07-10 20:23:47 +0000

    Fix unistall problem.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.LinuxMonitorAddOn"
-VERSION = "1.0"
+VERSION = "1.0.1"
 AUTHOR = "Eric Edgar"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
